b4216b59fe72fbd26f887d68c926506d0fc3d644
Nada-Elhossiny <Nadaaa.ee@gmail.com>
2022-06-21 22:09:28 -0400

    Create data.js

---
+++ frontend/src/data.js
@@ -0,0 +1,42 @@
+// Define data as an object and products as an array
+// Each product is an object
+const data = {
+  products: [
+    {
+      name: 'Moon lamp',
+      slug: 'moon-lamp',
+      category: 'Home-Decor',
+      image: '/images/lamp.jpg',
+      price: 29.99,
+      stock: 10,
+      brand: 'Brightworld',
+      rating: 4.8,
+      numReviews: 134,
+      description: 'Lunar lamp with wooden stand',
+    },
+    {
+      name: 'Moon lamp',
+      slug: 'moon-lamp',
+      category: 'Home-Decor',
+      image: '/images/lamp.jpg',
+      price: 29.99,
+      stock: 10,
+      brand: 'Brightworld',
+      rating: 4.8,
+      numReviews: 134,
+      description: 'Lunar lamp with wooden stand',
+    },
+    {
+      name: 'Moon lamp',
+      slug: 'moon-lamp',
+      category: 'Home-Decor',
+      image: '/images/lamp.jpg',
+      price: 29.99,
+      stock: 10,
+      brand: 'Brightworld',
+      rating: 4.8,
+      numReviews: 134,
+      description: 'Lunar lamp with wooden stand',
+    },
+  ],
+};
