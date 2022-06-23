a3bf547c57f06103a2d45f83a2245c4d39c84d2f
Nada-Elhossiny <Nadaaa.ee@gmail.com>
2022-06-22 22:52:37 -0400

    Add a route to the product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
-import data from './data';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <header>
-          <a href="/">Shopadora</a>
+          <Link to="/">Shopadora</Link>
         </header>
         <main>
           <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />}></Route>
             <Route path="/" element={<HomeScreen />} />
           </Routes>
         </main>
