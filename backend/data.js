f353da7fcd3a345e6c4f8b1a7b5b6f6dc56c1de4
Nada-Elhossiny <Nadaaa.ee@gmail.com>
2022-06-22 23:05:20 -0400

    Create a server that can be accessed at specified address

---
+++ backend/data.js
@@ -0,0 +1,44 @@
+// Define data as an object and products as an array
+// Each product is an object
+const data = {
+  products: [
+    {
+      name: 'Moon lamp',
+      slug: 'moon-lamp',
+      category: 'Home-Decor',
+      image: '/images/lamp.jpg',
+      price: 29.99,
+      stock: 10,
+      brand: 'Brightworld',
+      rating: 4.8,
+      numReviews: 134,
+      description: 'Lunar lamp with wooden stand',
+    },
+    {
+      name: 'Floral Maxi Dress',
+      slug: 'floral-maxi-dress',
+      category: 'Clothing',
+      image: '/images/maxi-dress.jpg',
+      price: 79.99,
+      stock: 20,
+      brand: 'Lulus',
+      rating: 4.5,
+      numReviews: 1500,
+      description: 'Blush Pink Floral Maxi Dress',
+    },
+    {
+      name: 'Virtual Reality Headset',
+      slug: 'VR-headset',
+      category: 'Technology',
+      image: '/images/VR-headset.jpg',
+      price: 259.99,
+      stock: 5,
+      brand: 'Oculus',
+      rating: 4,
+      numReviews: 189,
+      description: 'All in one Virtual Reality Headset',
+    },
+  ],
+};
+
+export default data;
