34584467196a5bf6b232f89801f6536195c7f7f7
Nada-Elhossiny <Nadaaa.ee@gmail.com>
2022-06-29 22:25:32 -0400

    retrieve product info from DB

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -15,10 +17,9 @@ mongoose
   });
 
 const app = express();
+app.use('/api/seed', seedRouter);
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
+app.use('/api/products', productRouter);
 
 app.get('/api/products/slug/:slug', (req, res) => {
   const product = data.products.find((x) => x.slug == req.params.slug);
