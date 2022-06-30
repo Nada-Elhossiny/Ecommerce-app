f8d875eacbd25d1205790df641f951d5908a7a24
Nada-Elhossiny <Nadaaa.ee@gmail.com>
2022-06-30 11:10:26 -0400

    fix api not connecting bug

--- frontend/src/screens/SignInScreen.js
+++ frontend/src/screens/SignInScreen.js
@@ -1,32 +1,57 @@
+import { Axios } from 'axios';
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import { Helmet } from 'react-helmet-async';
 import { Link, useLocation } from 'react-router-dom';
+import data from '../data';
 
 export default function SignInScreen() {
   const { search } = useLocation();
   const redirectURL = new URLSearchParams(search).get('redirect');
   const redirect = redirectURL ? redirectURL : '/';
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await Axios.post('/api/users/signin', {
+        email,
+        password,
+      });
+    } catch (err) {}
+  };
+  console.log(data);
   return (
     <Container className="small-container">
       <Helmet>
         <title>Sign In</title>
       </Helmet>
       <h1 className="my-3">Sign In</h1>
-      <Form>
+      <Form onSubmit={submitHandler}>
         <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" required />
+          <Form.Control
+            type="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="password">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" required />
+          <Form.Control
+            type="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
         <div className="mb-3">
           <Button type="submit">Sign In</Button>
         </div>
-        <div ClassName="mb-3">
+        <div className="mb-3">
           New to Shopadora?{' '}
           <Link to={`/signup?redirect=${redirect}`}>Create a New Account</Link>
         </div>
