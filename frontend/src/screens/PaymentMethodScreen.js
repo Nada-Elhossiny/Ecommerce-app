3128990a6a7ecc180244ccc8bc72314c1cf4d69a
Nada-Elhossiny <Nadaaa.ee@gmail.com>
2022-07-07 22:46:37 -0400

    Create PaymentMethodScreen.js

---
+++ frontend/src/screens/PaymentMethodScreen.js
@@ -0,0 +1,47 @@
+import React, { useContext, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Helmet } from 'react-helmet-async';
+import { useNavigate } from 'react-router-dom';
+import CheckoutPath from '../components/CheckoutPath';
+import { Store } from '../Store';
+
+export default function PaymentMethodScreen() {
+    const navigate = useNavigate();
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+      cart: {shippingAddress. paymentMethod},
+  }  = state;
+  useEffect(() => {
+      if (!shippingAddress.address) {
+          navigate('/shipping');
+      }
+  }, [shippingAddress, navigate])
+  submitHandler = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div>
+      <CheckoutPath step1 step2 step3></CheckoutPath>
+      <div className="container small-container">
+        <Helmet>
+          <title>Payment Method</title>
+        </Helmet>
+        <h1 className="my-3">Payment Method</h1>
+        <Form onSubmit={submitHandler}>
+          <div className="mb-3">
+            <Form.Check
+              type="radio"
+              id="paypal"
+              label="paypal"
+              value="paypal"
+              checked={paymentMethodName === 'PayPal'}
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            />
+          </div>
+          <Button type="submit">Continue</Button>
+        </Form>
+      </div>
+    </div>
+  );
+}
