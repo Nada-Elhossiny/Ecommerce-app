ddf9a1648ef1c7546601fb0d4bf6e3c905b6f886
Nada-Elhossiny <Nadaaa.ee@gmail.com>
2022-07-08 22:30:27 -0400

    Add place order screen to app

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import CheckoutPath from '../components/CheckoutPath';
 import CartScreen from './CartScreen';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import { Helmet } from 'react-helmet-async';
+import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { Store } from '../Store';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function PlaceOrderScreen() {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
   return (
     <div>
       <CheckoutPath step1 step2 step3 step4></CheckoutPath>
@@ -20,10 +26,10 @@ export default function PlaceOrderScreen() {
             <Card.Body>
               <Card.Title>Shipping</Card.Title>
               <Card.Text>
-                <strong>Name:</strong> {cart.shippingDetails.fullName} <br />
-                <strong>Address:</strong> {cart.shippingDetails.address},
-                {cart.shippingDetails.city},{cart.shippingDetails.country},
-                {cart.shippingDetails.postalCode}
+                <strong>Name:</strong> {cart.shippingAddress.fullName} <br />
+                <strong>Address:</strong> {cart.shippingAddress.address},
+                {cart.shippingAddress.city},{cart.shippingAddress.country},
+                {cart.shippingAddress.postalCode}
               </Card.Text>
               <Link to="/shipping">Edit</Link>
             </Card.Body>
