e7710e7bc90eded7ddaf68806fd290938c078195
Nada-Elhossiny <Nadaaa.ee@gmail.com>
2022-07-11 23:00:58 -0400

    implement cart clear

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -39,8 +39,10 @@ function reducer(state, action) {
         (item) => item._id !== action.payload._id
       );
       localStorage.setItem('cartItems', JSON.stringify(cartItems));
-      return { ...state, cart: { ...state.cart, cartItems } };
+      return { ...state, cart: { ...state.cart, cartItems: [] } };
     }
+    case 'CART_CLEAR':
+      return { ...state, userInfo: action.payload };
     case 'USER_SIGNIN':
       return { ...state, userInfo: action.payload };
     case 'USER_SIGNOUT':
