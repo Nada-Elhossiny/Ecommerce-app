3d039f78c1f7717498aac8b046fa3cef226f0ff7
Nada-Elhossiny <Nadaaa.ee@gmail.com>
2022-07-14 22:47:51 -0400

    Show shipping, payment and order information

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -1,8 +1,13 @@
 import React, { useContext, useEffect, useReducer } from 'react';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
-import Store from '../Store';
+import { Store } from '../Store';
 import { useNavigate, useParams } from 'react-router-dom';
+import Axios from 'axios';
+import { getError } from '../utils';
+import { Helmet } from 'react-helmet-async';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function reducer(state, action) {
   switch (action.type) {
@@ -32,19 +37,95 @@ export default function OrderScreen() {
   });
 
   useEffect(() => {
+    const fetchOrder = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await Axios.get(`/api/orders/${orderId}`, {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        });
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
     if (!userInfo) {
       return navigate('/login');
     }
     if (!order._id || (order._id && order._id !== orderId)) {
       fetchOrder();
     }
-  }, []);
+  }, [order, userInfo, orderId, navigate]);
 
   return loading ? (
     <LoadingBox></LoadingBox>
   ) : error ? (
     <MessageBox variant="danger">{error}</MessageBox>
   ) : (
-    <div></div>
+    <div>
+      <Helmet>
+        <title>Order {orderId}</title>
+      </Helmet>
+      <h1 className="my-3">Order {orderId}</h1>
+      <Row>
+        <Col md={8}>
+          <Card className="mb-3">
+            <Card.Body>
+              <Card.Title>Shipping Information</Card.Title>
+              <Card.Text>
+                <strong>Name:</strong> {order.shippingAddress.fullName} <br />
+                <strong>Address:</strong> {order.shippingAddress.address},
+                {order.shippingAddress.city}, {order.shippingAddress.country},
+                {order.shippingAddress.postalCode}
+              </Card.Text>
+              {order.isDelivered} ? (
+              <MessageBox variant="success">
+                Delivered at {order.deliveredAt}
+              </MessageBox>
+              ) : (<MessageBox variant="danger">Not Delivered</MessageBox>)
+            </Card.Body>
+          </Card>
+          <Card className="mb-3">
+            <Card.Body>
+              <Card.Title>Payment Information</Card.Title>
+              <Card.Text>
+                <strong>Method:</strong> {order.paymentMethod}
+              </Card.Text>
+              {order.isPaid ? (
+                <MessageBox variant="success">
+                  Paid at {order.paidAt}
+                </MessageBox>
+              ) : (
+                <MessageBox variant="danger">Awaiting Payment</MessageBox>
+              )}
+            </Card.Body>
+          </Card>
+          <Card className="mb-3">
+            <Card.Body>
+              <Card.Title>Ordered Items</Card.Title>
+              <ListGroup variant="flush">
+                {order.orderItems.map((item) => (
+                  <ListGroup.Item key={item._id}>
+                    <Row className="align-items-center">
+                      <Col md={6}>
+                        <img
+                          src={item.image}
+                          alt={item.name}
+                          className="img-fluid rounded img-thumbnail"
+                        ></img>{' '}
+                        <Link to={`/product/${item.slug}`}>{item.name}</Link>
+                      </Col>
+                      <Col md={3}>
+                        <span>{item.quantity}</span>
+                      </Col>
+                      <Col md={3}>${item.price}</Col>
+                    </Row>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </div>
   );
 }
